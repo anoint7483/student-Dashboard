a421db5d39ea772bea9bc03c2b3bf9a21d46fd57
anoint7483 <yabhishek8774@gmail.com>
2025-03-06 21:44:30 +0530

    some changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () =>{
                 {/* <img src="assets/images/react.svg" alt="" /> */}
             </div>
             <ul>
-                <li><a href="">Dashboard</a></li>
-                <li><a href="">Payment Info</a></li>
-                <li><a href="">Registration</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="">Assignments</a></li>
+                <li><a href="">Attendence</a></li>
                 <li><a href="">Courses</a></li>
-                <li><a href="">Drop semester</a></li>
-                <li><a href="">Result</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Results</a></li>
                 <li><a href="">Notice</a></li>
                 <li><a href="">Shedule</a></li>
             </ul>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,51 +1,64 @@
-// import './Sidebar.css';
+import { useState } from 'react';
 
 const Sidebar = () => {
+  const notices = [
+    {
+      title: 'Prelim Payment Due',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam explicabo.',
+      fileUrl: '/files/prelim-payment.pdf',
+    },
+    {
+      title: 'Exam Schedule',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur.',
+      fileUrl: '/files/exam-schedule.pdf',
+    },
+    {
+      title: 'Assignment Submission',
+      content: 'Please submit your assignment by the due date. Late submissions will not be accepted.',
+      fileUrl: '/files/assignment-submission.pdf',
+    },
+    // Add more notices as needed
+  ];
+
+  const handleDownload = (fileUrl) => {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileUrl.split('/').pop();
+    link.click();
+  };
+
   return (
-    <div className="sideBar">
-    <div className="course-instructors">
-        <div className="heading">
-            <h4>Course Instructors</h4>
-        </div>
-        <div className="instructors">
-            <div className="image"></div>
-            <div className="image"></div>
-            <div className="image"></div>
-        </div>
-    </div>
-    <div className="notice">
+    <div className="sidebar">
+      <div className="notice">
         <div className="head">
-            <div className="title">
-                <h4>Daily Notice</h4>
-            </div>
-            <div className="see-all">See all</div>
+          <div className="title">
+            <h4>Notices</h4>
+          </div>
         </div>
+
         <div className="content">
-            <div className="notice-update">
-                <div className="notice-card">
-                    <div className="content-title">
-                        <h5>Prelim Payment due</h5>
-                    </div>
-                    <div className="content-text">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam explicabo.
-                        </p>
-                    </div>
-                    <div className="see-more">See more</div>
-                </div>
-                <div className="notice-card">
-                    <div className="content-title">
-                        <h5>Exam Shedule</h5>
-                    </div>
-                    <div className="content-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur.
-                        </p>
-                    </div>
-                    <div className="see-more">See more</div>
-                </div>
+          {notices.map((notice, index) => (
+            <div
+              key={index}
+              className="notice-card"
+            >
+              <div className="content-title">
+                <h5>{notice.title}</h5>
+              </div>
+              <div className="content-text">
+                <p>{notice.content}</p>
+              </div>
+              <div
+                className="download-button"
+                onClick={() => handleDownload(notice.fileUrl)}
+              >
+                Download
+              </div>
             </div>
+          ))}
         </div>
+      </div>
     </div>
-</div>
   );
 };
 
